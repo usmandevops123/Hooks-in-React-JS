ccd2a7128852a3087de1cefd0586a395c35521f2
Muhammad Usman <61974138+usmandevops123@users.noreply.github.com>
2021-02-09 08:10:27 -0800

    upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react"; 
+
+let count=1;
+
+const App=()=>{  
+  const state =useState();
+  const [count, setCount] = useState(0);
+  const IncNum=()=>{ 
+    setCount(count + 1);
+     };
+  
+return (
+<> 
+<h1> {count} </h1> 
+
+<button onClick={IncNum}> Click Me </button>
+</>
+  );
+}; 
+export default App;
